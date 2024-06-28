2d6b6a0954e79449d56eeebdb0c6e3d1b59000f5
rabiavural98 <ozaktanr98@gmail.com>
2024-06-28 20:55:38 +0300

    Postman get post delete request ve response işlemleri yapıldı

--- ReactFundamental/vite-project/src/App.jsx
+++ ReactFundamental/vite-project/src/App.jsx
@@ -4,9 +4,11 @@ const App = () => {
     let [data,setData]=useState();
     
     useEffect(()=>{
-    fetch('https://dummyjson.com/products/1')
-        .then(res=>res.json())
-        .then(json=>setData(json))
+        (async()=> {
+         let response=await fetch('https://dummyjson.com/products/1')  
+          let json=await response.json()
+          setData(json)  
+        })()
     },[])
     return (
         <div>
