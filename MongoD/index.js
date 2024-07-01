3441ce7a34be6c590a888094679ef0d617a65157
rabiavural98 <ozaktanr98@gmail.com>
2024-07-01 21:14:55 +0300

    Mongodb ile veri oluşturma kaydetme işlemleri yapıldı

--- MongoD/index.js
+++ MongoD/index.js
@@ -1,2 +1,33 @@
 ﻿const mongoose =require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test').then(()=>console.log('Database Is Connected')).catch((err)=>console.log(err));
+
+//Schema (Shape of a Document)
+//Document,Collection,Database
+ const userSchema = new mongoose.Schema({
+     name: String,
+     age:Number,
+     isMarried:Boolean,
+     salary:Number,
+     gender:String,
+})
+ const User =mongoose.model('User',userSchema);
+ 
+ async function fetchInformation(){
+     const users =await User.find({isMarried:false}).select('name salary').sort('-salary').limit(2);
+     console.log(users);
+ }
+
+ fetchInformation();
+ 
+ // async function storeInformation(){
+ //     const user=new User({
+ //         name: 'Ariyan',
+ //         age:40,
+ //         isMarried:false,
+ //         salary:50000,
+ //         gender:'Male',
+ //     });
+ //     await user.save();
+ //     console.log(user);
+ // }
+ // storeInformation();
