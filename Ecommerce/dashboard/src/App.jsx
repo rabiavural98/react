fb9a85116f7e0a4544e3a4b8f4fd15d3ba47600f
rabiavural98 <ozaktanr98@gmail.com>
2024-07-03 15:46:09 +0300

    Login Register Ayarlamaları yapıldı

--- Ecommerce/dashboard/src/App.jsx
+++ Ecommerce/dashboard/src/App.jsx
@@ -1,11 +1,12 @@
+import Router from "./router/Router";
+import {useState} from "react";
+import publicRoutes from "./router/routes/publicRoutes";
+
+
 function App() {
-  return (
-      <div className="App">
-        <h1 className="text-3xl font-bold underline">
-          Kazi Ariyan
-        </h1>
-      </div>
-  );
+    const [allRoutes,setAllRoutes]=useState([...publicRoutes])
+    console.log(allRoutes)
+    return <Router allRoutes={allRoutes} />
 }
 
 export default App;
--- Ecommerce/dashboard/src/views/auth/Register.jsx
+++ Ecommerce/dashboard/src/views/auth/Register.jsx
@@ -2,8 +2,12 @@
 
 const Register = () => {
     return (
-        <div>
-         <h1>Register Page</h1>   
+        <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
+   <div className='w-[350px] text-[#ffffff] p-2'>
+       <div className='bg-[#6f68d1] p-4 rounded-md'>
+           <h2 className='text-xl mb-3 font-bold'>Welcome to Ecommerce</h2>
+       </div>
+   </div>
         </div>
     );
 };
