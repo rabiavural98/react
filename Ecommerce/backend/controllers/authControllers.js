ab3bb07ca449f1bfa5838e04689eaa0108bbecbf
rabiavural98 <ozaktanr98@gmail.com>
2024-07-09 11:06:47 +0300

    sunucuya bağlanıldı

--- Ecommerce/backend/controllers/authControllers.js
+++ Ecommerce/backend/controllers/authControllers.js
@@ -1,6 +1,13 @@
-﻿class AuthControllers{
+﻿const adminModel=require('../models/adminModel')
+class AuthControllers{
     admin_login=async (req,res)=>{
-        console.log(req.body)
+const {email,password}=req.body
+        try{
+        const admin=await adminModel.findOne({email}).select('+password')
+            console.log(admin)
+        }catch(error){
+    
+        }
     }
 }
 module.exports=new AuthControllers()
