e8046589fff10fc9694105a836f43e86cb0320d6
rabiavural98 <ozaktanr98@gmail.com>
2024-07-15 23:50:33 +0300

    deneme

--- Ecommerce/dashboard/src/layout/Header.jsx
+++ Ecommerce/dashboard/src/layout/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-          <h1>Header</h1>  
+          <div className='fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40'>
+              <div>
+                  
+              </div>
+          </div>
         </div>
     );
 };
