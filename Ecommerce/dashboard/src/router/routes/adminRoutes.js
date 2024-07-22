cf6dfb989feaa62795cc9d2ae6986aff8bea87ac
rabiavural98 <ozaktanr98@gmail.com>
2024-07-22 14:35:05 +0300

    Order sayfasının tasarımı yapıldı

--- Ecommerce/dashboard/src/router/routes/adminRoutes.js
+++ Ecommerce/dashboard/src/router/routes/adminRoutes.js
@@ -1,5 +1,6 @@
 import {lazy} from "react";
 const AdminDashboard=lazy(()=>import('../../views/admin/AdminDashboard'))
+const Category=lazy(()=>import('../../views/admin/Category'))
 const Orders=lazy(()=>import('../../views/admin/Orders'))
 
 export const adminRoutes = [
@@ -11,6 +12,11 @@ export const adminRoutes = [
     {
         path: 'admin/dashboard/orders',
         element: <Orders/>,
+        role: 'admin'
+    },
+    {
+        path: 'admin/dashboard/category',
+        element: <Category/>,
         role: 'admin'
     }
 ]
--- Ecommerce/dashboard/src/views/admin/Orders.jsx
+++ Ecommerce/dashboard/src/views/admin/Orders.jsx
@@ -6,13 +6,13 @@ import Pagination from "../Pagination";
 const Orders = () => {
     const [currentPage,setCurrentPage] = useState(1)
     const [searchValue,setSearchValue] = useState('')
-    const [parPage,setPerPage] = useState(5)
+    const [parPage,setParPage] = useState(5)
     const [show,setShow] = useState(false)
     return (
         <div className='px-2 lg:px-7 pt-5'>
             <div className='w-full p-4 bg-[#6a5fdf] rounded-md'>
                 <div className='flex justify-between items-center '>
-                    <select onChange={(e) => setPerPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500
+                    <select onChange={(e) => setParPage(parseInt(e.target.value))} className='px-4 py-2 focus:border-indigo-500
               outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
               text-[#d0d2d6]'>
                         <option value="5">5</option>
@@ -187,21 +187,20 @@ const Orders = () => {
                     </div>
                 </div>
 
-                
-                <div className='w-full flex justify-end mt-4 bottom-4 right-4' >
-                    
-           
-             <Pagination
-             pageNumber={currentPage}
-             setPageNumber={setCurrentPage}
-             totalItem={50}
-             parPage={parPage}
-             showItem={3}
-             />
-                </div>   
-            
-                
-                
+
+                <div className='w-full flex justify-end mt-4 bottom-4 right-4'>
+
+
+                    <Pagination
+                        pageNumber={currentPage}
+                        setPageNumber={setCurrentPage}
+                        totalItem={50}
+                        parPage={parPage}
+                        showItem={3}
+                    />
+                </div>
+
+
             </div>
         </div>
     );
