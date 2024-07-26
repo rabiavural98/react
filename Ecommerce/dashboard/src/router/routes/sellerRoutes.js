04154556d7646f34f7aa22ed31b3792d613da3f3
rabiavural98 <ozaktanr98@gmail.com>
2024-07-26 10:36:37 +0300

    Seller dashboard sayfa tasarımı yapıldı

--- Ecommerce/dashboard/src/router/routes/sellerRoutes.js
+++ Ecommerce/dashboard/src/router/routes/sellerRoutes.js
@@ -1,9 +1,15 @@
 import {lazy} from "react";
 const Home=lazy(()=>import( '../../views/Home'))
+const SellerDashboard=lazy(()=>import( '../../views/seller/SellerDashboard'))
 
 export const sellerRoutes = [{
     path: '/',
     element: <Home/>,
     ability: ['admin','seller']
-}
+},
+    {
+        path: '/seller/dashboard',
+        element: <SellerDashboard/>,
+        ability: ['seller']
+    }
 ]
