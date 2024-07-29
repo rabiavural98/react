2eb2788ecbdb1df9cd913b93918bc3777bbf8aa9
rabiavural98 <ozaktanr98@gmail.com>
2024-07-29 22:12:13 +0300

    add product sayfa tasarımı yapıldı

--- Ecommerce/dashboard/src/views/seller/AddProduct.jsx
+++ Ecommerce/dashboard/src/views/seller/AddProduct.jsx
@@ -49,11 +49,19 @@ const AddProduct = () => {
     
     const [cateShow,setCateShow]=useState(false)
     const [category,setCategory]=useState('')
-    const [allCategory,setAllCategory]=useState([])
+    const [allCategory,setAllCategory]=useState(categorys)
     const[searchValue,setSearchValue]=useState('')
     
+  //  const  ariyan='sfjdhjdhDSKGJKDGFD'
     const categorySearch=(e)=> {
         const value=e.target.value
+        setSearchValue(value) 
+        if(value){
+            let srcValue=allCategory.filter(c=>c.name.toLowerCase().indexOf(value.toLowerCase()) > -1)
+            setAllCategory(srcValue)
+        }else{
+            setAllCategory(categorys)
+        }
         
     }
     return (
@@ -73,7 +81,7 @@ const AddProduct = () => {
                                <input className='px-4 py-2 focus:border-indigo-500
                 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
                 text-[#d0d2d6]' onChange={inputHandle} value={state.name} type="text" name='name' id='name'
-                 placeholder='Product Name'/>
+                                      placeholder='Product Name'/>
                            </div>
 
 
@@ -95,37 +103,40 @@ const AddProduct = () => {
                        text-[#d0d2d6]'>
                            <div className='flex flex-col w-full gap-1 relative'>
                                <label htmlFor="category">Category</label>
-                               <input readOnly onClick={()=>setCateShow(!cateShow)} className='px-4 py-2 focus:border-indigo-500
+                               <input readOnly onClick={() => setCateShow(!cateShow)} className='px-4 py-2 focus:border-indigo-500
                 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
-                text-[#d0d2d6]' onChange={inputHandle} value={category} type="text"  id='category'
-                placeholder='Category'/>
-                               <div>
-                               
+                text-[#d0d2d6]' onChange={inputHandle} value={category} type="text" id='category'
+                                      placeholder='--select category--'/>
+
+
                                <div className={`absolute top-[101%] bg-[#475569] w-full 
-                               transition-all ${cateShow ? 'scale-100': 'scale-0' }`}>
+                               transition-all ${cateShow ? 'scale-100' : 'scale-0'}`}>
+
                                    <div className='w-full px-4 py-2 fixed'>
-                                       
-                                           <input onChange={categorySearch} className='px-3 py-1 w-full focus:border-indigo-500 outline-none
-                                            bg-transparent border border-slate-700 rounded-md text-[#d0d2d6] overflow-hidden' type="text"
-                                            placeholder='search'/>
-                                    
-                                                 
-                                       </div>
+                                       <input value={searchValue} onChange={categorySearch} className='px-3 py-1 w-full focus:border-indigo-500 outline-none
+                                            bg-transparent border border-slate-700 rounded-md text-[#d0d2d6] overflow-hidden'
+                                              type="text" placeholder='search'/>
 
-                                       <div className='pt-14'></div>
-                                       <div className='flex justify-start items-start flex-col h-[200px] overflow-x-scroll'></div>
+                                   </div>
+
+
+                                   <div className='pt-14'></div>
+                                   <div className='flex justify-start items-start flex-col h-[200px] overflow-x-scrool'>
                                        {
-                                           allCategory.map((c, i) => <span onClick={()=> {
-                                               setCateShow(false)
-                                               setCategory(c.name)
-                                               setSearchValue('')
-                                               setAllCategory(categorys)
+                                           allCategory.map((c, i) => <span
+                                               className={`px-4 py-2 hover:bg-indigo-500 hover:text-white hover:shadow-lg w-full cursor pointer ${category === c.name && 'bg-indigo-500'}`}
+                                               onClick={() => {
+                                                   setCateShow(false)
+                                                   setCategory(c.name)
+                                                   setSearchValue('')
+                                                   setAllCategory(categorys)
 
-                                           }}>{c.name} </span> )
-                                           
+                                               }}>{c.name} </span>)
+
                                        }
 
                                    </div>
+
                                </div>
                            </div>
 
@@ -141,7 +152,55 @@ const AddProduct = () => {
                                </div>
 
                            </div>
+
                        </div>
+
+
+                       <div className='flex flex-col mb-3 md:flex-row gap-4 w-full
+                       text-[#d0d2d6]'>
+                           <div className='flex flex-col w-full gap-1'>
+                               <label htmlFor="name">Price</label>
+                               <input className='px-4 py-2 focus:border-indigo-500
+                outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
+                text-[#d0d2d6]' onChange={inputHandle} value={state.price} type="number" name='price' id='price'
+                                      placeholder='price'/>
+                           </div>
+
+
+                           <div className='flex flex-col mb-3 md:flex-row gap-4 w-full
+                       text-[#d0d2d6]'>
+                               <div className='flex flex-col w-full gap-1'>
+                                   <label htmlFor="discount">Discount</label>
+                                   <input className='px-4 py-2 focus:border-indigo-500
+                outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
+                text-[#d0d2d6]' onChange={inputHandle} value={state.discount} type="number" name='discount'
+                                          id='discount'
+                                          placeholder='discount by %'/>
+                               </div>
+
+                           </div>
+                       </div>
+
+
+                       <div className='flex flex-col w-full gap-1'>
+                           <label htmlFor="description" className='text-[#d0d2d6]'>Description</label>
+                           <textarea className='px-4 py-2 focus:border-indigo-500
+                outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
+                text-[#d0d2d6]' onChange={inputHandle} value={state.description} name='price' id='price'
+                                     placeholder='description' name="description" id="description"  cols="10"  rows="5"></textarea>
+                           
+                           
+                {/*           <input className='px-4 py-2 focus:border-indigo-500*/}
+                {/*outline-none bg-[#6a5fdf] border border-slate-700 rounded-md*/}
+                {/*text-[#d0d2d6]' onChange={inputHandle} value={state.price} type="number" name='price' id='price'*/}
+                {/*                  placeholder='price'/>*/}
+                           
+                       </div>
+                       <div className='grid lg:grid-cols-4 grid-col-1 md:grid-cols-3'>
+                           
+                       </div>
+                       
+                       
 
 
                    </form>
