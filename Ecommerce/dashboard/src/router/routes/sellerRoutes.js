5f25fead6966da02fe1a5fa9f95be457d7be6cc1
rabiavural98 <ozaktanr98@gmail.com>
2024-07-29 08:45:23 +0300

    Hata düzeltildi

--- Ecommerce/dashboard/src/router/routes/sellerRoutes.js
+++ Ecommerce/dashboard/src/router/routes/sellerRoutes.js
@@ -1,6 +1,7 @@
 import {lazy} from "react";
 const Home=lazy(()=>import( '../../views/Home'))
 const SellerDashboard=lazy(()=>import( '../../views/seller/SellerDashboard'))
+const AddProduct=lazy(()=>import( '../../views/seller/AddProduct'))
 
 export const sellerRoutes = [{
     path: '/',
@@ -10,6 +11,11 @@ export const sellerRoutes = [{
     {
         path: '/seller/dashboard',
         element: <SellerDashboard/>,
+        ability: ['seller']
+    },
+    {
+        path: '/seller/dashboard/add-product',
+        element: <AddProduct/>,
         ability: ['seller']
     }
 ]
