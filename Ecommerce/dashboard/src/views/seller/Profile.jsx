926b5ccb7fd8d10e0fdc4dc64153d0e9db9b928b
rabiavural98 <ozaktanr98@gmail.com>
2024-08-12 19:50:48 +0300

    Hata düzeltildi ve profile sayfası tamamlandı

--- Ecommerce/dashboard/src/views/seller/Profile.jsx
+++ Ecommerce/dashboard/src/views/seller/Profile.jsx
@@ -129,51 +129,123 @@ const Profile = () => {
                               hover:shadow-yellow-500/50 absolute right-2 top-2 cursor-pointer'><FaRegEdit/></span>
 
                                     <div className='flex gap-2'>
-                                        <span>Name: </span>
-                                        <span>Ariyan Khan</span>
+                                        <span>Shop Name: </span>
+                                        <span>Easy Shop</span>
                                     </div>
 
                                     <div className='flex gap-2'>
-                                        <span>Email: </span>
-                                        <span>ariyan@gmail.com</span>
+                                        <span>Divission: </span>
+                                        <span>Dhaka</span>
                                     </div>
 
                                     <div className='flex gap-2'>
-                                        <span>Role: </span>
-                                        <span>Seller</span>
+                                        <span>District: </span>
+                                        <span>Rajbari</span>
                                     </div>
 
                                     <div className='flex gap-2'>
-                                        <span>Status: </span>
-                                        <span>Active</span>
+                                        <span>Sub District: </span>
+                                        <span>Vola</span>
                                     </div>
 
-                                    <div className='flex gap-2'>
-                                        <span>Payment Account: </span>
-                                        <p>
-                                            {
-                                                status === 'active' ? <span className='bg-green-500 text-white
-                                            text-xs cursor-pointer font-normal ml-2 px-2 py-0.5 rounded'>Pending</span> :
-                                                    <span className='bg-blue-500 text-white
-                                            text-xs cursor-pointer font-normal ml-2 px-2 py-0.5 rounded'>Click Active </span>
-                                            }
-                                        </p>
-                                    </div>
                                 </div>
                             }
+                            
                         </div>
-
-
+                        
+                        
+                        
+                        
                     </div>
                 </div>
 
                 <div className='w-full md:w-6/12'>
+            <div className='w-full pl-0 md:pl-7 mt-6 md:mt-0 '>
+                <div className='bg-[#6a5fdf] rounded-md text-[#d0d2d6] p-4'>
+                    <h1 className='text-[#d0d2d6] text-lg mb-3 font-semibold'>Change Password</h1>
+                    <form>
+                        <div className='flex flex-col mb-3 w-full gap-1 mb-2'>
+                            <label htmlFor="email">Email</label>
+                            <input className='px-4 py-2 focus:border-indigo-500
+                outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
+                text-[#d0d2d6]' type="email" name='email' id='email' placeholder='email'/>
+                        </div>
+
+                        <div className='flex flex-col mb-3 w-full gap-1 mb-2'>
+                            <label htmlFor="o_password">Old Password</label>
+                            <input className='px-4 py-2 focus:border-indigo-500
+                outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
+                text-[#d0d2d6]' type="password" name='old_password' id='o_password' placeholder='Old Password'/>
+                        </div>
 
 
+                        <div className='flex flex-col mb-3 w-full gap-1 mb-2'>
+                            <label htmlFor="n_password">New Password</label>
+                            <input className='px-4 py-2 focus:border-indigo-500
+                outline-none bg-[#6a5fdf] border border-slate-700 rounded-md
+                text-[#d0d2d6]' type="password" name='new_password' id='n_password' placeholder='New Password'/>
+                        </div>
+
+
+                        {/*        <div className='flex flex-col mb-3 w-full gap-1 mb-2'>*/}
+                        {/*            <label htmlFor="subdis">Sub District Name</label>*/}
+                        {/*            <input className='px-4 py-2 focus:border-indigo-500*/}
+                        {/*outline-none bg-[#6a5fdf] border border-slate-700 rounded-md*/}
+                        {/*text-[#d0d2d6]' type="text" name='subdis' id='subdis' placeholder='Sub District Name'/>*/}
+                        {/*        </div>*/}
+
+                        <button className='bg-red-500 hover:shadow-red-500/40
+                                   hover:shadow-md text-white rounded-md px-7 py-2 my-2'>Save Changes
+                        </button>
+
+                    </form>
                 </div>
+
+
+                {/*<form>*/}
+                {/*    <div className='flex flex-col mb-3 w-full gap-1 mb-2'>*/}
+                {/*        <label htmlFor="Shop">Shop Name</label>*/}
+                {/*        <input className='px-4 py-2 focus:border-indigo-500*/}
+                {/*outline-none bg-[#6a5fdf] border border-slate-700 rounded-md*/}
+                {/*text-[#d0d2d6]' type="text" name='shopName' id='Shop' placeholder='Shop Name'/>*/}
+                {/*    </div>*/}
+                
+                {/*    <div className='flex flex-col mb-3 w-full gap-1 mb-2'>*/}
+                {/*        <label htmlFor="division">Division Name</label>*/}
+                {/*        <input className='px-4 py-2 focus:border-indigo-500*/}
+                {/*outline-none bg-[#6a5fdf] border border-slate-700 rounded-md*/}
+                {/*text-[#d0d2d6]' type="text" name='division' id='division' placeholder='Division Name'/>*/}
+                {/*    </div>*/}
+                
+                
+                {/*    <div className='flex flex-col mb-3 w-full gap-1 mb-2'>*/}
+                {/*        <label htmlFor="district">District Name</label>*/}
+                {/*        <input className='px-4 py-2 focus:border-indigo-500*/}
+                {/*outline-none bg-[#6a5fdf] border border-slate-700 rounded-md*/}
+                {/*text-[#d0d2d6]' type="text" name='district' id='district' placeholder='District Name'/>*/}
+                {/*    </div>*/}
+                
+                
+                {/*    <div className='flex flex-col mb-3 w-full gap-1 mb-2'>*/}
+                {/*        <label htmlFor="subdis">Sub District Name</label>*/}
+                {/*        <input className='px-4 py-2 focus:border-indigo-500*/}
+                {/*outline-none bg-[#6a5fdf] border border-slate-700 rounded-md*/}
+                {/*text-[#d0d2d6]' type="text" name='subdis' id='subdis' placeholder='Sub District Name'/>*/}
+                {/*    </div>*/}
+                
+                {/*    <button className='bg-red-500 hover:shadow-red-500/40*/}
+                {/*                   hover:shadow-md text-white rounded-md px-7 py-2 my-2'>Save Changes*/}
+                {/*    </button>*/}
+                
+                {/*</form>*/}
+            </div>
+                
+                
+                </div>
+                
+                
             </div>
         </div>
-
     );
 };
 
