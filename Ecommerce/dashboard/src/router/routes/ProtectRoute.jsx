cd0ce234df2cee5f6684ee2c4a510e54508a4d94
rabiavural98 <ozaktanr98@gmail.com>
2024-08-19 19:56:55 +0300

    Değişiklikler kaydedildi

--- Ecommerce/dashboard/src/router/routes/ProtectRoute.jsx
+++ Ecommerce/dashboard/src/router/routes/ProtectRoute.jsx
@@ -4,20 +4,51 @@
 //
 // const ProtectRoute = ({route,children}) => {
 //     const {role,userInfo}=useSelector(state=>state.auth)
-//    
+//
 //     if(role) {
 //         if(userInfo){
 //             if(userInfo.role===route.role){
+//                   
+//       if(route.status==='userInfo.status') {
 //                 return <Suspense fallback={null}>{children}</Suspense>
+//
 //             }else{
-//                 return <Navigate to='/unauthorized' replace />
+//     if(userInfo.status==='pending'){
+//         return <Navigate to='/seller/account-pending' replace/>
+//     }else{
+//         return <Navigate to='/seller/account-deactive' replace/>
+//
+//     }
+// }
+//
+//
+//                // return <Suspense fallback={null}>{children}</Suspense>
+//             }else{
+//                 if(route.visibility){
+//                 if(route.visibility.some(r=>r===userInfo.status)){
+//                     return <Suspense fallback={null}>{children}</Suspense>
+//                 }else{
+//                     return <Navigate to='/seller/account-pending' replace/>
+//
+//                 }
+//               if(route.visibility.some(r=>r===userInfo.status)){
+// //                 return <Suspense fallback={null}>{children}</Suspense>
+// } else{
+//             return <Navigate to='/seller/account-pending' replace/>
+//
+// }
+//               }else{
+//                 return <Suspense fallback={null}>{children}</Suspense>
+//               }
 //             }
-//         }
-//            
+//         }else{
+//       return <Navigate to='/unauthorized' replace />
+//          }
+//
 //     }else{
 //         return <Navigate to='/login' replace />
 //     }
-//   
+//
 // };
 //
 // export default ProtectRoute;
