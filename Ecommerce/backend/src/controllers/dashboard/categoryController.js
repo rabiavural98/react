3bbaa116b66841debd48584380aa8d61dcd8694d
rabiavural98 <ozaktanr98@gmail.com>
2024-08-28 09:49:12 +0300

    Hata giderildi çıktı alındı

--- Ecommerce/backend/src/controllers/dashboard/categoryController.js
+++ Ecommerce/backend/src/controllers/dashboard/categoryController.js
@@ -8,65 +8,13 @@ class categoryController {
 add_category = async (req, res) => {
    console.log('this is working')
 
-    
-    console.log(req.body) // req parametresini kullanma örneği
-    res.send('Request received') // res parametresini kullanma örneği
 
-
-
-
-    //  const form = new formidable()
-   //  form.parse(req,async(err, fields, files) => {
-   //      // console.log(fields)
-   //      // console.log(files)
-   //      if(err){
-   //          responseReturn(res,404,{error:'something went wrong'})
-   //      }else{
-   //          let {name} = fields
-   //          let {image} = files
-   //          name= name.trim()
-   //          const slug=name.split('').join('-')
-   //         
-   //         
-   //          cloudinary.config({
-   //             
-   //              cloud_name: process.env.cloud_name,
-   //              api_key: process.env.api_key,
-   //              api_secret: process.env.api_secret,
-   //              secure:true
-   //          }) 
-   //         
-   //          try{
-   //             const result= await cloudinary.uploader.upload(image.filepath,{folder:'categorys'})
-   //         
-   //              if(result){
-   //              const category= await categoryModel.create({
-   //                  name,
-   //                  slug,
-   //                  image:result.url
-   //              })
-   //                  responseReturn(res,201,{category,message:'Category Added Successfully'})
-   //
-   //              }else{
-   //              responseReturn(res,404,{error:'Image Upload File'})
-   //
-   //          }
-   //         
-   //          }catch{
-   //              responseReturn(res,500,{error:'Internal Server Error'})
-   //
-   //          }
-   //      }
-   //  })
-   // }
-   //
-   //
-   //  get_category = async (req, res) => {
-   //      console.log('this is working')
-    
-    
-    }
-   
 }
 
-    module.exports=new categoryController()
+}
+
+module.exports=new categoryController()
+
+   
+    
+
--- Ecommerce/backend/src/routes/dashboard/categoryRoutes.js
+++ Ecommerce/backend/src/routes/dashboard/categoryRoutes.js
@@ -3,7 +3,10 @@ const router = require('express').Router()
 const categoryController=require('../../controllers/dashboard/categoryController')
 const {authMiddleware} = require('../../middlewares/authMiddleware')
 
-router.post('/category-add',authMiddleware,categoryController.add_category)
+
+router.post('/category-add',categoryController.add_category)
+
+//router.post('/category-add',authMiddleware,categoryController.add_category)
 // router.post('/category-get',authMiddleware,categoryController.get_category)
 
 module.exports=router
