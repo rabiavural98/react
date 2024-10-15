6d601ab61b2aaf78608056e6d6b67486cf80ee6a
rabiavural98 <ozaktanr98@gmail.com>
2024-10-15 21:26:08 +0300

    Seller Request Show Add

--- Ecommerce/backend/src/controllers/dashboard/sellerController.js
+++ Ecommerce/backend/src/controllers/dashboard/sellerController.js
@@ -2,14 +2,31 @@
 const Formidable=require('formidable').IncomingForm;
 const {responseReturn} = require("../../utilities/response")
 const cloudinary = require('cloudinary').v2
-const categoryModel = require('../../models/categoryModel')
+const sellerModel = require('../../models/sellerModel')
+const categoryModel = require("../../models/categoryModel");
 
 
 class sellerController {
 
    
     request_seller_get = async (req, res) => {
-        console.log(req.query)
+        //console.log(req.query)
+        const {parPage, page, searchValue} = req.query
+        const skipPage = parseInt(parPage) * (parseInt(page) - 1)
+
+        
+        try{
+            if (searchValue) {
+            
+            }else{
+            const sellers = await sellerModel.find({status: 'pending'}).skip(skipPage).limit(parPage).sort({createdAt: -1})
+            const totalSeller = await sellerModel.find({ status: 'pending'}).countDocuments()
+            responseReturn(res, 500, { error: error.message})
+
+            }
+        }catch(error){ 
+            
+        }
     }
     //end method
 
