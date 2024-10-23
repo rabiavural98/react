b72f0e30a0b776cda8046660def8fd8f8074df76
rabiavural98 <ozaktanr98@gmail.com>
2024-10-23 12:58:00 +0300

    Folder Route Manage

---
+++ Ecommerce/frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import Header from "../components/Header";
+
+const Home = () => {
+    return (
+        <div className='w-full'>
+            <Header/>
+        </div>
+    );
+};
+
+export default Home;
